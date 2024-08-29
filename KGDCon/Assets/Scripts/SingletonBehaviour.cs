c0f019aa36d7254c9463d45c238d986543e919f9
huijae <t4r@naver.com>
2024-08-29 11:24:09 +0900

    singleton 추가

---
+++ KGDCon/Assets/Scripts/SingletonBehaviour.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+
+    protected virtual void Awake()
+    {
+        if (_instance == null)
+            _instance = this as T;
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
