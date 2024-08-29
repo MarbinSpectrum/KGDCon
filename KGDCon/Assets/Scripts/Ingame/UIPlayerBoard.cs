2e5a2e38b4141e233672a1b73ab33e22ab81b5a1
huijae <t4r@naver.com>
2024-08-29 14:33:39 +0900

    UI 추가

---
+++ KGDCon/Assets/Scripts/Ingame/UIPlayerBoard.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPlayerBoard : SingletonBehaviour<UIPlayerBoard>
+{
+    public int Score
+    {
+        get => _score;
+        set
+        {
+            _score = Mathf.Max(value, 0);
+            UpdateScore(_score);
+        }
+    }
+    public bool IsGameover => _lifeIndex >= 2 * _lives.Length;
+
+    [SerializeField] private TextMeshProUGUI _scoreText;
+    [SerializeField] private Image[] _lives;
+
+    private int _score;
+    private int _lifeIndex;
+
+    public void Initialize()
+    {
+        Score = 0;
+        _lifeIndex = 0;
+        UpdateScore(0);
+        for (int i = 0; i < _lives.Length; ++i)
+            _lives[i].fillAmount = 1f;
+    }
+
+    private void UpdateScore(int newScore) =>
+        _scoreText.text = $"Score: {newScore:#,##0}";
+
+    public void IncreaseHalfLife()
+    {
+        if (_lifeIndex <= 0)
+            return;
+        _lives[--_lifeIndex / 2].fillAmount += 0.5f;
+    }
+
+    public void DecreaseLife()
+    {
+        DecreaseHalfLife();
+        DecreaseHalfLife();
+    }
+
+    public void DecreaseHalfLife()
+    {
+        if (IsGameover)
+            return;
+        _lives[_lifeIndex++ / 2].fillAmount -= 0.5f;
+    }
+}
--- KGDCon/Assets/Scripts/SingletonBehaviour.cs
+++ KGDCon/Assets/Scripts/SingletonBehaviour.cs
@@ -4,18 +4,12 @@ using UnityEngine;
 
 public class SingletonBehaviour<T> : MonoBehaviour where T : MonoBehaviour
 {
+    public static T Instance => _instance;
     private static T _instance;
 
     protected virtual void Awake()
     {
         if (_instance == null)
             _instance = this as T;
-        else if (_instance != this)
-        {
-            Destroy(gameObject);
-            return;
-        }
-
-        DontDestroyOnLoad(gameObject);
     }
 }
