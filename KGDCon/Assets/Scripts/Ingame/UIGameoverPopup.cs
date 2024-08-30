7106415504e94b7b9d4dc1748b819a1985336ceb
KangYoungMook <9507ym@naver.com>
2024-08-30 13:56:52 +0900

    goto main

--- KGDCon/Assets/Scripts/Ingame/UIGameoverPopup.cs
+++ KGDCon/Assets/Scripts/Ingame/UIGameoverPopup.cs
@@ -15,10 +15,9 @@ public class UIGameoverPopup : UI
         {
             Sfx.Instance.PlayButtonClick();
             Canvas.Instance.Get<UIStartPopup>().Bind();
-            Time.timeScale = 1f;
             Hide();
 
-            GameSystem.Instance.StartGame();
+            Canvas.Instance.Get<UIStartPopup>().Show();
         }
     }
 
